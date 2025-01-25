d14428eeed50a5a264d4fb34770e7578b5a24a7a
Alexandre Philibert <alexandre.philibert@ik.me>
2025-01-25 15:09:05 +0100

    Things :tm:

--- code/tests/modelbridgemanager.h
+++ code/tests/modelbridgemanager.h
@@ -2,15 +2,76 @@
 #define MODELBRIDGEMANAGER_H
 
 #include <iostream>
+#include <utility>
 
 #include "pcomodel.h"
 #include "pcosynchro/pcosemaphore.h"
 #include "pcoconcurrencyanalyzer.h"
 
+class BridgeManager
+{
+    PcoSemaphore mutex;
+    PcoSemaphore waitingAccess;
+    int nbWaitingAccess;
+    float currentWeight;
+    float maxWeight;
+
+public:
+    BridgeManager(float maxWeight) : mutex(1), waitingAccess(0)
+    {
+        this->maxWeight = maxWeight;
+        currentWeight = 0;
+        nbWaitingAccess = 0;
+    }
+
+    void access(float weight)
+    {
+        startSection(1);
+
+        mutex.acquire();
+
+        while (currentWeight + weight > maxWeight) {
+            startSection(2);
+            nbWaitingAccess++;
+            mutex.release();
+            waitingAccess.acquire();
+            mutex.acquire();
+        }
+
+        currentWeight += weight;
+
+        if (nbWaitingAccess > 0) {
+            nbWaitingAccess--;
+            waitingAccess.release();
+        }
+        startSection(3);
+
+        mutex.release();
+
+    }
+
+    void leave(float weight)
+    {
+        mutex.acquire();
+        currentWeight -= weight;
+        if (nbWaitingAccess > 0) {
+            nbWaitingAccess--;
+            waitingAccess.release();
+        }
+
+        mutex.release();
+    }
+};
+
+
 class ThreadCar : public ObservableThread
 {
+private:
+    BridgeManager& bridgeManager;
+    float WEIGHT = 3.0f;
+
 public:
-    explicit ThreadCar(std::string id = "") : ObservableThread(std::move(id))
+    explicit ThreadCar(BridgeManager &bridgeManager, std::string id = "") : bridgeManager(bridgeManager), ObservableThread(std::move(id))
     {
         scenarioGraph = std::make_unique<ScenarioGraph>();
         auto scenario = scenarioGraph->createNode(this, -1);
@@ -19,44 +80,61 @@ public:
         auto p3 = scenarioGraph->createNode(this, 3);
         scenario->next.push_back(p1);
         p1->next.push_back(p2);
+        p1->next.push_back(p3);
+        p2->next.push_back(p2);
         p2->next.push_back(p3);
         scenarioGraph->setInitialNode(scenario);
     }
 
 private:
-    static std::unique_ptr<PcoSemaphore> semaphore;
     void run() override
     {
+        bridgeManager.access(WEIGHT);
+        bridgeManager.leave(WEIGHT);
         endScenario();
     }
 };
 
 class ThreadTruck : public ObservableThread
 {
+private:
+    BridgeManager& bridgeManager;
+    float WEIGHT = 30.0f;
+
 public:
-    explicit ThreadTruck(std::string id = "") : ObservableThread(std::move(id))
+    explicit ThreadTruck(BridgeManager &bridgeManager, std::string id = "") : bridgeManager(bridgeManager), ObservableThread(std::move(id))
     {
         scenarioGraph = std::make_unique<ScenarioGraph>();
         auto scenario = scenarioGraph->createNode(this, -1);
-        auto p1 = scenarioGraph->createNode(this, 4);
-        auto p2 = scenarioGraph->createNode(this, 5);
-        auto p3 = scenarioGraph->createNode(this, 6);
+        auto p1 = scenarioGraph->createNode(this, 1);
+        auto p2 = scenarioGraph->createNode(this, 2);
+        auto p3 = scenarioGraph->createNode(this, 3);
         scenario->next.push_back(p1);
         p1->next.push_back(p2);
+        p1->next.push_back(p3);
+        p2->next.push_back(p2);
         p2->next.push_back(p3);
         scenarioGraph->setInitialNode(scenario);
     }
 
 private:
-    static std::unique_ptr<PcoSemaphore> semaphore;
     void run() override
     {
+        bridgeManager.access(WEIGHT);
+        startSection(2);
+        bridgeManager.leave(WEIGHT);
         endScenario();
     }
 };
 
 class ModelBridgeManager : public PcoModel
 {
+private:
+    BridgeManager bridgeManager;
+
+public:
+    ModelBridgeManager(): bridgeManager(60.0f) {}
+
     bool checkInvariants() override 
     {
         return true;
@@ -65,8 +143,9 @@ class ModelBridgeManager : public PcoModel
     
     void build() override
     {
-        threads.emplace_back(std::make_unique<ThreadCar>("car"));
-        threads.emplace_back(std::make_unique<ThreadTruck>("truck"));
+        threads.emplace_back(std::make_unique<ThreadCar>(bridgeManager, "car"));
+        threads.emplace_back(std::make_unique<ThreadTruck>(bridgeManager, "truck1"));
+        threads.emplace_back(std::make_unique<ThreadTruck>(bridgeManager, "truck2"));
 
         scenarioBuilder = std::make_unique<ScenarioBuilderBuffer>();
         scenarioBuilder->init(threads, 9);
@@ -92,62 +171,5 @@ class ModelBridgeManager : public PcoModel
     }
 };
 
-
-class BridgeManagerFloat
-{
-public:
-    virtual void access(float weight) = 0;
-    virtual void leave(float weight) = 0;
-};
-
-class BridgeManager : public BridgeManagerFloat
-{
-    PcoSemaphore mutex;
-    PcoSemaphore waitingAccess;
-    PcoSemaphore fifo;
-    int nbWaitingAccess;
-    float currentWeight;
-    float maxWeight;
-    float waitingWeight;
-
-public:
-    BridgeManager(float maxWeight) : mutex(1), waitingAccess(0), fifo(1)
-    {
-        this->maxWeight = maxWeight;
-        currentWeight = 0.0;
-        nbWaitingAccess = 0;
-        waitingWeight = 0.0;
-    }
-
-    void access(float weight) override
-    {
-        fifo.acquire();
-        mutex.acquire();
-
-        // Because of the if, it is wrong
-        if (currentWeight + weight > maxWeight) {
-            waitingWeight = currentWeight;
-            mutex.release();
-            waitingAccess.acquire();
-        }
-
-        currentWeight += weight;
-        mutex.release();
-        fifo.release();
-    }
-
-    void leave(float weight) override
-    {
-        mutex.acquire();
-        currentWeight -= weight;
-        if ((waitingWeight > 0.0) && (currentWeight + waitingWeight <= maxWeight)) {
-            waitingWeight = 0.0;
-            waitingAccess.release();
-        }
-        else {
-            mutex.release();
-        }
-    }
-};
 
 #endif // MODELBRIDGEMANAGER_H
